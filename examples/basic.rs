7b417efe60d1e9872bcb413aaced280c22d61304
Tim 'Piepmatz' Hesse <git+github@cptpiepmatz.de>
2025-10-22 13:38:37 +0200

    improved basic example to output stuff

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,8 +1,8 @@
-use std::borrow::Cow;
+use std::{borrow::Cow, io::{self, Stdout, Write}};
 
 use kitest::{
     filter::NoFilter,
-    formatter::NoFormatter,
+    formatter::{FmtTestOutcome, TestFormatter},
     ignore::NoIgnore,
     meta::{TestFnHandle, TestMeta},
     panic_handler::NoPanicHandler,
@@ -30,6 +30,35 @@ const TESTS: &[TestMeta] = &[
     },
 ];
 
+struct BasicFormatter(Stdout);
+
+struct BasicTestOutcome {
+    name: Cow<'static, str>,
+}
+
+impl From<FmtTestOutcome<'_, '_, ()>> for BasicTestOutcome {
+    fn from(value: FmtTestOutcome<'_, '_, ()>) -> Self {
+        BasicTestOutcome { name: value.meta.name.clone() }
+    }
+}
+
+impl TestFormatter<()> for BasicFormatter {
+    type RunInit = ();
+    fn fmt_run_init(&mut self, _: Self::RunInit) -> std::io::Result<()> {
+        writeln!(self.0, "started testing")
+    }
+
+    type TestOutcome = BasicTestOutcome;
+    fn fmt_test_outcome(&mut self, data: Self::TestOutcome) -> std::io::Result<()> {
+        writeln!(self.0, "test {:?} done", data.name)
+    }
+
+    type RunStart = ();
+    type TestIgnored = ();
+    type TestStart = ();
+    type RunOutcomes = ();
+}
+
 fn main() {
     kitest::run_tests(
         TESTS.iter(),
@@ -37,6 +66,6 @@ fn main() {
         SimpleRunner,
         NoIgnore,
         NoPanicHandler,
-        NoFormatter,
+        BasicFormatter(io::stdout()),
     );
 }
