4c4c9c5121ca3cd49b18205557b3cc349aa83c9b
Tim 'Piepmatz' Hesse <git+github@cptpiepmatz.de>
2025-11-02 11:39:05 +0100

    add tests for default ignore

--- src/strategy/ignore/default.rs
+++ src/strategy/ignore/default.rs
@@ -24,5 +24,66 @@ impl<Extra> TestIgnore<Extra> for DefaultIgnore {
     }
 }
 
+#[cfg(test)]
+mod tests {
+    use std::{borrow::Cow, ops::Deref, sync::LazyLock};
+
+    use super::*;
+    use crate::{outcome::TestStatus, test::Test, test_support::*};
+
+    // This can be done without a LazyLock at const time but requires more characters.
+    static TESTS: LazyLock<[Test; 3]> = LazyLock::new(|| {
+        [
+            test! {name: "ok", ignore: false},
+            test! {name: "ignored", ignore: true},
+            test! {name: "ignored_with_reason", ignore: "with reason"},
+        ]
+    });
+
+    #[test]
+    fn ignoring_works() {
+        let report = harness(TESTS.deref())
+            .with_ignore(DefaultIgnore::default())
+            .run();
+
+        assert_eq!(report.outcomes.len(), 3);
+        assert!(report.outcomes[0].1.status.passed());
+        assert!(report.outcomes[1].1.status.ignored());
+        assert!(report.outcomes[2].1.status.ignored());
+
+        assert!(matches!(
+            report.outcomes[1].1.status,
+            TestStatus::Ignored { reason: None }
+        ));
+        assert!(matches!(
+            report.outcomes[2].1.status,
+            TestStatus::Ignored {
+                reason: Some(Cow::Borrowed("with reason"))
+            }
+        ));
+    }
+
+    #[test]
+    fn include_ignored_works() {
+        let report = harness(TESTS.deref())
+            .with_ignore(DefaultIgnore::IncludeIgnored)
+            .run();
+
+        assert_eq!(report.outcomes.len(), 3);
+        assert!(report.outcomes[0].1.status.passed());
+        assert!(report.outcomes[1].1.status.passed());
+        assert!(report.outcomes[2].1.status.passed());
+    }
+
+    #[test]
+    fn ignored_only_works() {
+        let report = harness(TESTS.deref())
+            .with_ignore(DefaultIgnore::IgnoredOnly)
+            .run();
+
+        assert_eq!(report.outcomes.len(), 3);
+        assert!(report.outcomes[0].1.status.ignored());
+        assert!(report.outcomes[1].1.status.passed());
+        assert!(report.outcomes[2].1.status.passed());
     }
 }
